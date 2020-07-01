25fc94d21df9b8d9e48aa7a555744daa8cc928b1
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-07-01 19:50:40 -0400

    Add the rest of the bookmarks app

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import AddBookmark from './AddBookmark/AddBookmark';
+import BookmarkList from './BookmarkList/BookmarkList';
+import Nav from './Nav/Nav';
+import config from './config';
+import './App.css';
+
+const bookmarks = [
+  // {
+  //   id: 0,
+  //   title: 'Google',
+  //   url: 'http://www.google.com',
+  //   rating: '3',
+  //   desc: 'Internet-related services and products.'
+  // },
+  // {
+  //   id: 1,
+  //   title: 'Thinkful',
+  //   url: 'http://www.thinkful.com',
+  //   rating: '5',
+  //   desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
+  // },
+  // {
+  //   id: 2,
+  //   title: 'Github',
+  //   url: 'http://www.github.com',
+  //   rating: '4',
+  //   desc: 'brings together the world\'s largest community of developers.'
+  // }
+];
+
+class App extends Component {
+  state = {
+    //page: 'list',
+    bookmarks,
+    error: null,
+  };
+
+  /*changePage = (page) => {
+    this.setState({ page })
+  }*/
+
+  setBookmarks = bookmarks => {
+    this.setState({
+      bookmarks,
+      error: null,
+      //page: 'list',
+    })
+  }
+
+  addBookmark = bookmark => {
+    this.setState({
+      bookmarks: [ ...this.state.bookmarks, bookmark ],
+    })
+  }
+
+  componentDidMount() {
+    fetch(config.API_ENDPOINT, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${config.API_KEY}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
+        return res.json()
+      })
+      .then(this.setBookmarks)
+      .catch(error => this.setState({ error }))
+  }
+
+  render() {
+    const { bookmarks } = this.state
+    return (
+      <main className='App'>
+        <h1>Bookmarks!</h1>
+        <Nav />
+        <div className='content' aria-live='polite'>
+          <Route
+            path='/add-bookmark'
+            render={({ history }) => {
+              return <AddBookmark
+                  onAddBookmark={this.addBookmark}
+                  onClickCancel={() => history.push('/')}
+              />
+            }}
+          />
+          <Route
+            exact 
+            path= '/'
+            render={() =>
+              <BookmarkList
+                bookmarks={bookmarks}
+              />
+            }
+          />
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+  //API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$1i5.HIc14xYL83wtrG9A8etpFDfQC19TOm0dh3vdvMisEn7LK.s8y',
+}
